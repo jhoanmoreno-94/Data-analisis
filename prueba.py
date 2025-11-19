948ebca7739bf0d2f137df7718af897bdfcc69fd
SebastianM <jhoan.moreno@udea.edu.co>
2025-11-18 19:48:38 -0500

    Cambio

--- prueba.py
+++ prueba.py
@@ -1,1 +1,8 @@
-2+2
+import pandas as pd
+vendedores = ["Ana", "Luis", "Marta", "Sergio"]
+ventas = [1500.50, 2300.75, 1800.00, 2100.25]
+df = pd.DataFrame({
+    "Vendedor": vendedores, 
+    "Ventas": ventas
+})  
+print(df)
